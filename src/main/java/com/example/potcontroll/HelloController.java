b23be8e9294b55115e77887b629d223a35928c66
Dimitrios Tsakos <s0578980@htw-berlin.de>
2024-04-17 11:04:09 +0200

    Rest Service hinzugefügt

--- src/main/java/com/example/potcontroll/HelloController.java
+++ src/main/java/com/example/potcontroll/HelloController.java
@@ -1,15 +1,26 @@
 package com.example.potcontroll;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // https://spring.io/guides/gs/spring-boot
 @RestController
 public class HelloController {
 
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
     @GetMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 
 }
