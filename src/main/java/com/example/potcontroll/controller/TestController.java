7da55f64acf1f9ae5220e57731a22f3569702782
Dimitrios Tsakos <s0578980@htw-berlin.de>
2024-05-28 12:12:49 +0200

    Testcontroller added

---
+++ src/main/java/com/example/potcontroll/controller/TestController.java
@@ -0,0 +1,27 @@
+package com.example.potcontroll.controller;
+
+import com.example.potcontroll.data.TestEntry;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class TestController {
+
+    private final List<TestEntry> testEntryList;
+
+    public TestController() {
+        testEntryList = List.of(
+                new TestEntry(1L, "Test1", "Description1"),
+                new TestEntry(2L, "Test2", "Description2"),
+                new TestEntry(3L, "Test3", "Description3")
+        );
+    }
+
+    @GetMapping(path= "/api/testEntryList")
+    public ResponseEntity<List<TestEntry>> getTestEntryList() {
+        return ResponseEntity.ok(testEntryList);
+    }
+}
