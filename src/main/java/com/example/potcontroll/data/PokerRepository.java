7c9bf9a4ec6b167a73a2d88fbb2f1ac32ad8ed83
Dimitrios Tsakos <s0578980@htw-berlin.de>
2024-05-29 09:59:19 +0200

    PokerRepository

--- src/main/java/com/example/potcontroll/data/PokerRepository.java
+++ src/main/java/com/example/potcontroll/data/PokerRepository.java
@@ -1,4 +1,3 @@
-/*
 package com.example.potcontroll.data;
 
 import org.springframework.data.repository.CrudRepository;
@@ -7,4 +6,3 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PokerRepository extends CrudRepository<PokerGameEntry, Long> {
 }
-*/
