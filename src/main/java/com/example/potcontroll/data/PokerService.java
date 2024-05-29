02fe8c53fb68762631ff694741e008e3ae1f40d4
Dimitrios Tsakos <s0578980@htw-berlin.de>
2024-05-29 10:08:54 +0200

    PokerService Setup

--- src/main/java/com/example/potcontroll/data/PokerService.java
+++ src/main/java/com/example/potcontroll/data/PokerService.java
@@ -1,8 +1,10 @@
-/*
 package com.example.potcontroll.data;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class PokerService {
@@ -17,5 +19,26 @@ public class PokerService {
     public PokerGameEntry get(Long id) {
         return repo.findById(id).orElseThrow(RuntimeException::new);
     }
+
+    public List<PokerGameEntry> getAllWithoutOwner() {
+        Iterable<PokerGameEntry> iterator = repo.findAll();
+        List<PokerGameEntry> pokerGameEntryList = new ArrayList<PokerGameEntry>();
+        for(PokerGameEntry entry : iterator) {
+            if(entry.getOwner().isEmpty() || entry.getOwner() == null) {
+                pokerGameEntryList.add(entry);
+            }
+        }
+        return pokerGameEntryList;
+    }
+
+    public List<PokerGameEntry> getAllOwnedBy(String owner) {
+        Iterable<PokerGameEntry> iterator = repo.findAll();
+        List<PokerGameEntry> pokerGameEntryList = new ArrayList<PokerGameEntry>();
+        for(PokerGameEntry entry : iterator) {
+            if(entry.getOwner() != null || entry.getOwner().equals(owner)) {
+                pokerGameEntryList.add(entry);
+            }
+        }
+        return pokerGameEntryList;
+    }
 }
-*/
