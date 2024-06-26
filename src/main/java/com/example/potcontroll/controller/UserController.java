8f1f9ee5c53a6edb9319d95b9dd4293d7bc13e5b
Dimitrios Tsakos <s0578980@htw-berlin.de>
2024-06-26 15:06:06 +0200

    logger info hinzugefügt

--- src/main/java/com/example/potcontroll/controller/UserController.java
+++ src/main/java/com/example/potcontroll/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
 import java.util.List;
 
 @RestController
@@ -17,45 +18,59 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    /*
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+     */
+
     Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @CrossOrigin
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody User user) {
+        logger.info("Registering user: {}", user.getUsername());
         List<User> allreadyRegisteredUsers = userService.getAll();
         for(User registeredUser : allreadyRegisteredUsers) {
             if(registeredUser.getUsername().equals(user.getUsername())) {
                 return new ResponseEntity<>("User already exists", HttpStatus.BAD_REQUEST);
             }
         }
+        // user.setPassword(passwordEncoder.encode(user.getPassword()));
         userService.save(user);
+        logger.info("User registered: {}", user.getUsername());
         return new ResponseEntity<>(user.getUsername(), org.springframework.http.HttpStatus.OK);
     }
 
     @CrossOrigin
     @PostMapping("/login")
     public ResponseEntity<String> loginUser(@RequestBody User user) {
+        logger.info("Logging in user: {}", user.getUsername());
         List<User> allreadyRegisteredUsers = userService.getAll();
         for(User registeredUser : allreadyRegisteredUsers) {
             if(registeredUser.getUsername().equals(user.getUsername())) {
                 if(registeredUser.getPassword().equals(user.getPassword())) {
+                    logger.info("User logged in: {}", user.getUsername());
                     return new ResponseEntity<>(user.getUsername(), org.springframework.http.HttpStatus.OK);
                 }
             }
         }
+        logger.info("User not found for login: {}", user.getUsername());
         return new ResponseEntity<>("User not found", HttpStatus.BAD_REQUEST);
     }
 
     @CrossOrigin
     @DeleteMapping("/user/{username}")
     public ResponseEntity<String> deleteUser(@PathVariable String username) {
+        logger.info("Deleting user: {}", username);
         List<User> allreadyRegisteredUsers = userService.getAll();
         for(User registeredUser : allreadyRegisteredUsers) {
             if(registeredUser.getUsername().equals(username)) {
                 userService.delete(registeredUser.getId());
+                logger.info("User deleted: {}", username);
                 return new ResponseEntity<>(username, org.springframework.http.HttpStatus.OK);
             }
         }
+        logger.info("User not found for delete: {}", username);
         return new ResponseEntity<>("User not found", HttpStatus.BAD_REQUEST);
     }
 
